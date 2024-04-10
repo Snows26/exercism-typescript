3d943a86e5b700f0e6e10b955a8ee7491cb5149e
Snows26 <matheusneve26@gmail.com>
2024-04-10 17:37:24 -0300

    2 exercise

---
+++ two-fer/two-fer.ts
@@ -0,0 +1,9 @@
+/**
+ * This stub is provided to make it straightforward to get started.
+ */
+
+export function twoFer(nameFriends?: string): string {
+
+  return nameFriends ? "One for " + nameFriends + ", one for me." : "One for you, one for me."
+
+}
